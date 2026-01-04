f7bae357b8ecf13314a9c6bc25307c74a19b4cbe
rosegaipnazarova <rosegaipnazarova@gmail.com>
2026-01-04 22:44:39 +0500

    js

--- task.js
+++ task.js
@@ -5,7 +5,6 @@
 //         this.name = name
 //         this.age = age
 //     }
-
 //     greet() {
 //         return `Salom, mening ismim ${this.name} va yoshim ${this.age}da`
 //     }
@@ -19,12 +18,10 @@
 //     constructor(balance = 0) {
 //         this.balance = balance
 //     }
-
 //     deposit(amount) {
 //         this.balance += amount;
 //         return `balans: ${this.balance}`
 //     }
-
 //     withdraw(amount) {
 //         if (amount > this.balance) {
 //             return "Mablag' yetarli emas!"
@@ -34,27 +31,41 @@
 //         }
 //     }
 // }
-
 // const myacc = new BankAccount(1000)
 // console.log(myacc.deposit(200));    
 // console.log(myacc.withdraw(300));   
 
 //n3
 
-class Rectangle {
-    constructor(width, height) {
-        this.width = width
-        this.height = height
-    }
+// class Rectangle {
+//     constructor(width, height) {
+//         this.width = width
+//         this.height = height
+//     }
+//     area() { 
+//         return this.width * this.height
+//     }
+//     perimetr() { 
+//         return 2 * (this.width + this.height)
+//     }
+// }
 
-    area() { 
-        return this.width * this.height
-    }
-    perimetr() { 
-        return 2 * (this.width + this.height)
-    }
-}
+// const shakl = new Rectangle(8, 6)
+// console.log(shakl.area());      
+// console.log(shakl.perimetr()); 
 
-const shakl = new Rectangle(8, 6)
-console.log(shakl.area());      
-console.log(shakl.perimetr()); 
+//n4
+
+// class Student {
+//     constructor(name, id, grades = []) {
+//         this.name = name
+//         this.id = id
+//         this.grades = grades
+//     }
+//     calculateAverage() {
+//         let sum = this.grades.reduce((a, b) => a + b, 0)
+//         return sum / this.grades.length
+//     }
+// }
+// const talaba = new Student("rosie", "ID1111", [5, 4, 3, 4])
+// console.log(`${talaba.name}ning o'rtacha bahosi: ${talaba.calculateAverage()}`);
